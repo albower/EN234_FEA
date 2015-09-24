4012367672fed8f237eec82f2feaf37ec496ca7b
Bower <Allan_Bower@brown.edu>
2015-09-23 20:39:43 -0400

    Changed array dimensions of spatial shape function derivatives in element utilities

--- EN234_FEA/user_codes/modules/Element_Utilities.f90
+++ EN234_FEA/user_codes/modules/Element_Utilities.f90
@@ -12,11 +12,11 @@ module Element_Utilities
   
     real (prec) :: shape_function_spatial_derivatives_1D(3,1)
     real (prec) :: shape_function_spatial_derivatives_2D(9,2)
-    real (prec) :: shape_function_spatial_derivatives_3D(27,3)
+    real (prec) :: shape_function_spatial_derivatives_3D(20,3)
 
     real (prec) :: vol_avg_shape_function_derivatives_1D(3,1)
     real (prec) :: vol_avg_shape_function_derivatives_2D(9,2)
-    real (prec) :: vol_avg_shape_function_derivatives_3D(27,3)
+    real (prec) :: vol_avg_shape_function_derivatives_3D(20,3)
   
     real (prec) :: integrationpoints_1D(6,1)
     real (prec) :: integrationpoints_2D(2,9)
