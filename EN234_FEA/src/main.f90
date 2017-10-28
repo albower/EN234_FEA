efc84e7668869f5a073b6d908f9ba7f4b487922f
dongli314 <32434921+dongli314@users.noreply.github.com>
2017-10-27 21:10:36 -0400

    HW6 - nonlinear user material

--- EN234_FEA/src/main.f90
+++ EN234_FEA/src/main.f90
@@ -89,13 +89,13 @@ program en234fea
 
 !  HW5  Cantilever beam to test incompatible mode elements
 
-   infil = 'input_files/Abaqus_uel_cantilever.in'
-   outfil = 'Output_files/Abaqus_uel_cantilever.out'
+   !infil = 'input_files/Abaqus_uel_cantilever.in'
+   !outfil = 'Output_files/Abaqus_uel_cantilever.out'
 
 !  HW6  Porous elasticity UMAT
 
-!   infil = 'input_files/Abaqus_umat_porous_elastic.in'
-!   outfil = 'Output_files/Abaqus_umat_porous_elastic.out'
+   infil = 'input_files/Abaqus_umat_porous_elastic.in'
+   outfil = 'Output_files/Abaqus_umat_porous_elastic.out'
 
 !  HW7 Hyperelastic user element
 !   infil = 'input_files/Abaqus_uel_hyperelastic.in'
